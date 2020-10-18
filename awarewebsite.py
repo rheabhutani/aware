3e1a58735a6af32b94c2a3e0d7045066242d384d
Arjun Singla <arjun0999@gmail.com>
2020-10-18 04:15:14 -0700

    changed folder name and mods

--- awarewebsite.py
+++ awarewebsite.py
@@ -1,21 +1,28 @@
+from flask import Flask, render_template
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
+import sys
+app = Flask(__name__)
 
 
-MAX_SEQUENCE_LENGTH = 5000
-MAX_NUM_WORDS = 25000
-EMBEDDING_DIM = 300
-TEST_SPLIT = 0.2
+@app.route('/')
+def result():
+	x = "The coronavirus has killed more than 200,000 people in the United States alone."
+	return predict([x])
 
-tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
-model = tf.keras.models.load_model("trainedModel.h5")
-
-# text_input needs to be an array of one element, which contains the text of the article
 def predict(text_input):
+	MAX_SEQUENCE_LENGTH = 5000
+	MAX_NUM_WORDS = 25000
+	EMBEDDING_DIM = 300
+	TEST_SPLIT = 0.2
+
+	tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+	model = tf.keras.models.load_model("trainedModel.h5")
+
 	tokenizer.fit_on_texts([text_input[0]])
 	sequences = tokenizer.texts_to_sequences([text_input[0]])
 
@@ -25,24 +32,15 @@ def predict(text_input):
 	                     maxlen=MAX_SEQUENCE_LENGTH, 
 	                     padding='pre', 
 	                 truncating='pre')
+	print(data[0])
 	integervalue = int(np.rint(model.predict( np.array( [data[0],] )  ))[0][0])
 	if integervalue == 0:
 		return "fake"
 	return "real"
 
+@app.route('/about')
+def about():
+	return render_template('index.html')
 
-print(predict(["The coronavirus has killed more than 200,000 people in the United States alone."]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run()
